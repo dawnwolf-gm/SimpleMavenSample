d2bfbb4c785a013028f08e8c56df38d5e75d8eaa
MTR\240913 <dawnwolf@gmail.com>
2024-09-23 10:27:14 +0900

    빌드 실패 테스트

--- ch-simple/simple/src/main/java/org/sonatype/mavenbook/App.java
+++ ch-simple/simple/src/main/java/org/sonatype/mavenbook/App.java
@@ -6,7 +6,7 @@ package org.sonatype.mavenbook;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main1( String[] args )
     {
         System.out.println( "Hello World New!" );
     }
